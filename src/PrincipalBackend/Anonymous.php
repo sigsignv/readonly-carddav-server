7c04906c44d7c095adffe1f8722ab4edfbcfae16
Sigsign <sig@signote.cc>
2022-08-14 13:35:36 +0900

    Add PrincipalBackend/Anonymous.php

---
+++ src/PrincipalBackend/Anonymous.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cross\PrincipalBackend;
+
+use Sabre\DAVACL\PrincipalBackend\AbstractBackend;
+
+class Anonymous extends AbstractBackend
+{
+    public function getPrincipalsByPrefix($prefixPath)
+    {
+        $principals = [];
+        if ($prefixPath === 'principals') {
+            $principals[] = [
+                'uri' => 'principals/anonymous',
+                '{http://sabredav.org/ns}email-address' => 'anonymous@example.com',
+                '{DAV:}displayname' => 'Anonymous',
+            ];
+        }
+        return $principals;
+    }
+
+    public function getPrincipalByPath($path)
+    {
+        $principal = [];
+        if ($path === 'principals/anonymous') {
+            $principal = [
+                'id' => 1,
+                'uri' => 'principals/anonymous',
+                '{http://sabredav.org/ns}email-address' => 'anonymous@example.com',
+                '{DAV:}displayname' => 'Anonymous',
+            ];
+        }
+        return $principal;
+    }
+
+    public function updatePrincipal($path, \Sabre\DAV\PropPatch $propPatch)
+    {
+        // Update always failed
+        $propPatch->handle([], function () {
+            return false;
+        });
+    }
+
+    public function searchPrincipals($prefixPath, array $searchProperties, $test = 'allof')
+    {
+        if ($prefixPath !== 'principals' || empty($searchProperties)) {
+            return [];
+        }
+        return ['principals/anonymous'];
+    }
+
+    public function getGroupMemberSet($principal)
+    {
+        return [];
+    }
+
+    public function getGroupMembership($principal)
+    {
+        return [];
+    }
+
+    public function setGroupMemberSet($principal, array $members)
+    {
+        return;
+    }
+}
